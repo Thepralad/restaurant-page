8dcf51f2bb1a3b956d06f32ac4f2c281a950e57c
thepralad <prgurung04@gmail.com>
2024-06-18 21:44:55 +0530

    chore: Configure webpack for bundling assets and add HTML, CSS, and JavaScript files for the restaurant page

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { Template } = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test : /\.css$/i,
+                use : ['style-loader', 'css-loader'],
+            }
+        ]
+    }
+}
