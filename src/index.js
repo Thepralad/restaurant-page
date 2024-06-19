41806027c8f33b9c795688780575046e15ec2bd8
thepralad <prgurung04@gmail.com>
2024-06-19 15:33:01 +0530

    feat: Add navigation functionality to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import './style.css';
-console.log('this page works')
+import { homePage } from './home';
+import { menuPage } from './menu';
+import { contactPage } from './contact';
+homePage();
+document.getElementById('home').addEventListener('click', () =>{
+    document.getElementById('content').innerHTML = '';
+    homePage();
+});
+document.getElementById('menu').addEventListener('click', () =>{
+    document.getElementById('content').innerHTML = '';
+    menuPage();
+});
+document.getElementById('contact').addEventListener('click', () =>{
+    document.getElementById('content').innerHTML = '';
+    contactPage();
+});
+
